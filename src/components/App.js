4dd2f749ffd51c2df5d5e958004afc7c7d92e573
Owen Craig <owenkcraig@gmail.com>
2018-07-27 15:21:35 -0400

    In progress.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../styles/App.css';
 import axios from "axios";
-import Login from './login.js';
 import Editor from './editor.js';
 import BattleDisplay from './battle-display.js';
 
@@ -10,14 +9,52 @@ class App extends Component {
   state = {
       user: [],
       battles: [],
-      loading: false
+      loading: false,
+      loginUser: "",
+      newUser: ""
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   }
 
   loadUsers = async () => {
-    this.setState({ loading: true })
-    const userRes = await axios.get('/users');
-    const user = userRes.data.payload[0]
-    this.setState({ user, loading: false })
+    // this.setState({ loading: true })
+    // const userRes = await axios.get('/users');
+    // const user = userRes.data.payload[0]
+    // this.setState({ user, loading: false })
+  }
+
+  loginUser = async (e) => {
+    // this.setState({ loading: true })
+    const checkUser = this.state.loginUser;
+    const checkingUser = await axios.get(`/user/${checkUser}`);
+    console.log(checkingUser);
+    // this.setState({ loading: false })
+
+    // if (checkUser === checkingUser) {
+    //   console.log("samesies")
+    // } else {
+    //   console.log("nope");
+    // }
+
+
+    //search the database for this value
+    //if found then return it and set it to state
+      //refresh page
+    //else return user not found
+  }
+
+  signUpUser = async (e) => {
+    const body = {
+      name: this.state.newUserName,
+      email: this.state.newUserEmail
+    }
+    axios.post("/usersPost", body).then(res => {
+      this.props.history.push('/');
+    }).catch( err => console.log(err))
   }
 
   refresh = () => {
@@ -32,11 +69,15 @@ class App extends Component {
   render() {
     const { user, loading } = this.state;
     if (loading) return (<h1>Loading...</h1>)
-    if (!user) return (
+    if (user === undefined || user.length === 0) return (
       <div className="battle-skry-not-logged-in">
-        <h1>Not logged in.</h1>
-        <Login />
-        <button onClick={this.refresh}>TEST</button>
+        <h2>Have an account already?</h2>
+        <input className='battle-skry-login-user' onChange={this.onChange} name="loginUser" placeholder="Email Address" />
+        <button className="battle-skry-login-button" type="button" onClick={this.loginUser}>Login</button>
+        <h2>Or sign up!</h2>
+        <input className='battle-skry-signup-user' onChange={this.onChange} name="newUserName" placeholder="Username" />
+        <input className='battle-skry-signup-user-email' onChange={this.onChange} name="newUserEmail" placeholder="Email Address" />
+        <button className="battle-skry-signup-button" type="button" onClick={this.signUpUser}>Sign Up</button>
       </div>
     )
     return (
--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Login = props => {
-  const { handleChange, LoginUser, userName } = props
+  const { handleChange, LoginUser, SignUpUser, userName } = props
   return (
     <div className='battle-skry-login'>
-      <input className='battle-skry-login-user' onChange={handleChange} value={userName} />
-      <button className="battle-skry-login-button" type="button" onClick={LoginUser}>Login</button>
+    	<h2>Have an account already?</h2>
+      	<input className='battle-skry-login-user' onChange={handleChange} value={userName} />
+      	<button className="battle-skry-login-button" type="button" onClick={LoginUser}>Login</button>
+      	<h2>Or sign up!</h2>
+      	<input className='battle-skry-signup-user' onChange={handleChange} value={userName} />
+      	<button className="battle-skry-signup-button" type="button" onClick={SignUpUser}>Sign Up</button>
     </div>
   )
 }
